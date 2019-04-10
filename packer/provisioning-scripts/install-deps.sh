ed5d3164fb75c7df8d83fe3f08a67aa17a7435dc
John O'Sullivan <john@eximchain.com>
2019-04-10 12:18:51 -0400

    Laid out skeleton to build docker image with our deps pre-installed.

---
+++ packer/provisioning-scripts/install-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eu -o pipefail
+
+sudo apt-get update
+sudo apt-get install -y build-essential npm
+# Add repository for current version of node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
