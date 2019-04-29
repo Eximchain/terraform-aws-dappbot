3f9cfa85277353035f5d0de0663b8819b609ad9f
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-04-29 14:24:54 -0400

    Fix missing } in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,7 @@ output "api_dns" {
 
 output "cloudfront_cert_arn" {
     value = "${element(coalescelist(aws_acm_certificate.cloudfront_cert.*.arn, list("")), 0)}"
+}
 
 output "cognito_client_id" {
     value = "${aws_cognito_user_pool_client.api_client.id}"
