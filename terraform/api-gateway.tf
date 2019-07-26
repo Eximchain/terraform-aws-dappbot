5f6b7431096c51c48b54baa07e7b7c5418cefd5f
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-07-26 13:04:46 -0400

    Move API Gateway deployments into separate file

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -6,43 +6,6 @@ resource "aws_api_gateway_rest_api" "dapp_api" {
   description = "Proxy to handle requests to the Dappbot & Dapphub API"
 }
 
-resource "aws_api_gateway_deployment" "dapp_api_deploy_v1" {
-  depends_on = [
-    aws_api_gateway_integration.dappbot_public_proxy_any,
-    aws_api_gateway_method.dappbot_public_proxy_any,
-
-    aws_api_gateway_integration.dappbot_private_proxy_any,
-    aws_api_gateway_method.dappbot_private_proxy_any,
-
-    aws_api_gateway_integration.dappbot_private_get,
-    aws_api_gateway_method.dappbot_private_get,
-
-    aws_api_gateway_integration.dappbot_private_proxy_cors,
-    aws_api_gateway_method.dappbot_private_proxy_cors,
-
-    aws_api_gateway_integration.dappbot_private_cors,
-    aws_api_gateway_method.dappbot_private_cors,
-
-    aws_api_gateway_integration.dappbot_auth_proxy_any,
-    aws_api_gateway_method.dappbot_auth_proxy_any,
-
-    aws_api_gateway_integration.payment_stripe_any,
-    aws_api_gateway_method.payment_stripe_any,
-
-    aws_api_gateway_integration.payment_stripe_post,
-    aws_api_gateway_method.payment_stripe_post,
-
-    aws_api_gateway_integration.payment_stripe_cors,
-    aws_api_gateway_method.payment_stripe_cors,
-
-    aws_api_gateway_integration.payment_stripe_webhook_any,
-    aws_api_gateway_method.payment_stripe_webhook_any
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.dapp_api.id
-  stage_name  = "v1"
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # API GATEWAY: `/private/` DAPPBOT API
 # ---------------------------------------------------------------------------------------------------------------------
